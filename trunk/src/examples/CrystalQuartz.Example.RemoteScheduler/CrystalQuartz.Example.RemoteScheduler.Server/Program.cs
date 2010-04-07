3f7d09b300080c7a27388ee3b43cd89910e383e2
GuryanovEV <GuryanovEV@74354d7b-ab83-0adf-8c1c-c52e8326750c>
2010-04-07 19:41:56 +0000

    ADDED: Remote scheduler provider. ADDED: Remote scheduler example.

---
+++ trunk/src/examples/CrystalQuartz.Example.RemoteScheduler/CrystalQuartz.Example.RemoteScheduler.Server/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace CrystalQuartz.Example.RemoteScheduler.Server
+{
+    using System;
+    using System.Collections.Specialized;
+    using Quartz;
+    using Quartz.Impl;
+    using Quartz.Job;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var properties = new NameValueCollection();
+            properties["quartz.scheduler.instanceName"] = "RemoteServerScheduler";
+
+            // set thread pool info
+            properties["quartz.threadPool.type"] = "Quartz.Simpl.SimpleThreadPool, Quartz";
+            properties["quartz.threadPool.threadCount"] = "5";
+            properties["quartz.threadPool.threadPriority"] = "Normal";
+
+            // set remoting expoter
+            properties["quartz.scheduler.exporter.type"] = "Quartz.Simpl.RemotingSchedulerExporter, Quartz";
+            properties["quartz.scheduler.exporter.port"] = "555";
+            properties["quartz.scheduler.exporter.bindName"] = "QuartzScheduler";
+            properties["quartz.scheduler.exporter.channelType"] = "tcp";
+
+            ISchedulerFactory sf = new StdSchedulerFactory(properties);
+            IScheduler sched = sf.GetScheduler();
+
+            // define the job and ask it to run
+            JobDetail job = new JobDetail("remotelyAddedJob", "default", typeof(NoOpJob));
+            JobDataMap map = new JobDataMap();
+            map.Put("msg", "Your remotely added job has executed!");
+            job.JobDataMap = map;
+            CronTrigger trigger = new CronTrigger("remotelyAddedTrigger", "default", "remotelyAddedJob", "default", DateTime.UtcNow, null, "/5 * * ? * *");
+
+            // schedule the job
+            sched.ScheduleJob(job, trigger);
+
+            sched.Start();
+        }
+    }
+}
